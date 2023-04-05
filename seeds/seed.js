74bdb037abf8a8fe51dc4fd17798c0c747a20437
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-04-05 12:33:03 -0600

    Add seeds

---
+++ seeds/seed.js
@@ -0,0 +1,33 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: 1,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: 1,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
